f542391972192f28580f4d0e37b91c9a29272aee
Ben Slusky <sluskyb@paranoiacs.org>
2018-04-11 23:32:03 -0400

    Technology prototypes

---
+++ data.lua
@@ -0,0 +1,85 @@
+data:extend
+({
+  {
+    type = "technology",
+    name = "parallax-scanning-1",
+    icon = "__ParallaxScanning__/radar-parallax.png",
+    icon_size = 128,
+    effects =
+    {
+      {
+        type = "nothing",
+        effect_description = {"effect-description.parallax-scanning-1"}
+      }
+    },
+    prerequisites = {"military-3"},
+    unit =
+    {
+      count = "150",
+      ingredients =
+      {
+        {"military-science-pack", 1},
+        {"science-pack-1", 1},
+        {"science-pack-2", 1},
+        {"science-pack-3", 1},
+      },
+      time = 30
+    },
+    order = "e-i"
+  },
+  {
+    type = "technology",
+    name = "parallax-scanning-2",
+    icon = "__ParallaxScanning__/radar-parallax.png",
+    icon_size = 128,
+    effects =
+    {
+      {
+        type = "nothing",
+        effect_description = {"effect-description.parallax-scanning-2"}
+      }
+    },
+    prerequisites = {"parallax-scanning-1"},
+    unit =
+    {
+      count = "500",
+      ingredients =
+      {
+        {"military-science-pack", 1},
+        {"science-pack-1", 1},
+        {"science-pack-2", 1},
+        {"science-pack-3", 1},
+      },
+      time = 30
+    },
+    order = "e-i"
+  },
+  {
+    type = "technology",
+    name = "parallax-scanning-3",
+    icon = "__ParallaxScanning__/radar-parallax.png",
+    icon_size = 128,
+    effects =
+    {
+      {
+        type = "nothing",
+        effect_description = {"effect-description.parallax-scanning-3"}
+      }
+    },
+    prerequisites = {"parallax-scanning-2"},
+    unit =
+    {
+      count = "1000",
+      ingredients =
+      {
+        {"military-science-pack", 1},
+        {"science-pack-1", 1},
+        {"science-pack-2", 1},
+        {"science-pack-3", 1},
+        {"high-tech-science-pack", 1},
+      },
+      time = 30
+    },
+    order = "e-i"
+  },
+})
